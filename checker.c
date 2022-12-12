15aec1e8cab354f88851826ca8c00df053c19926
KallappaM <115877818+KallappaM@users.noreply.github.com>
2022-12-12 12:17:02 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
-typedef unsigned char  boolean;
+
 #define RangeCheck(X,Y,Z) ((Z<X || Z>Y)? 0 : 1)
 #define MAXVALUE -32767
 
-boolean Check_Temperature(float temperature);
-boolean Check_SOC(float soc);
-boolean Check_ChargeRate(float chargeRate);
+int Check_Temperature(float temperature);
+int Check_SOC(float soc);
+int Check_ChargeRate(float chargeRate);
 
 int batteryIsOk(float temperature, float soc, float chargeRate) 
 {
-  boolean batterystate = 1;
+  int batterystate = 1;
   batterystate = Check_Temperature(temperature);
   batterystate = Check_SOC(soc);
   batterystate = Check_ChargeRate(chargeRate);
@@ -18,17 +18,17 @@ int batteryIsOk(float temperature, float soc, float chargeRate)
   return batterystate;
 }
 
-boolean Check_Temperature(float temperature)
+int Check_Temperature(float temperature)
 {
 	return RangeCheck(0 , 45, temperature);
 }
 
-boolean Check_SOC(float soc)
+int Check_SOC(float soc)
 {
 	return RangeCheck(20, 80, soc);
 }
 
-boolean Check_ChargeRate(float chargeRate)
+int Check_ChargeRate(float chargeRate)
 {
 	return RangeCheck(MAXVALUE , 0.8, chargeRate);
 }
@@ -38,4 +38,5 @@ int main()
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 }
+
 
