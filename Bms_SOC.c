08cf155592c5bab4847d5f0e3de7f51a187fb1f9
KallappaM <115877818+KallappaM@users.noreply.github.com>
2022-12-15 22:41:46 +0530

    Create Bms_SOC.c

---
+++ Bms_SOC.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <BMS_Cfg.h>
+
+/*Function returns flase if the SOC is out of range and true if its in range*/
+int Check_SOC(float soc)
+{
+  return RangeCheck(soc,SOCMIN,SOCMAX);
+
+}
+ 
+/*Function returns flase if the chargeRate is out of range and true if its in range*/ 
+int Check_ChargeRate(float chargeRate)
+{
+   if(chargeRate>CHARGERATEMAX)
+   return 0;
+   else
+   return 1;
+}
