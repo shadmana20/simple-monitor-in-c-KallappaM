c15e27c97df62ce62d2631c6349caf6feef8ac1d
KallappaM <115877818+KallappaM@users.noreply.github.com>
2022-12-18 18:44:51 +0530

    Create BatteryManagmentSystem.h

---
+++ BatteryManagmentSystem.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+typedef enum {
+    LOW_SOC_BREACH,
+    LOW_SOC_WARNING,
+    SOC_NORMAL,
+    HIGH_SOC_WARNING,
+    HIGH_SOC_BREACH,
+    SOC_UNDEFINED,
+    MESSAGE_MAX
+} WarningRanges;
+
+
+typedef enum {
+    DEFAULT,
+    ENGLISH,
+    GERMAN,
+    CHINA,
+	MAX_LANG
+}Select_Language;
+
+
+typedef enum {
+    Approaching_Discharge,
+    Approaching_Peak,
+    Warning_max
+} WarningWithTolerance;
+
+struct 
+{
+	int DrainRange;
+	int PeakRange;
+}descharge_range;
+
+void CalculateDrianPeakThreshold(int MinThreshold , int MaxThreshold);
+void SelectLanguageandWarnigMessage(Select_Language Language);
+WarningRanges BatteryHelathMonitor(int soc);
